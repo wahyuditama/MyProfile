1ffcf5c88ad4d6f69ce412ecdedd0a8f00a5abce
MRizkyHakiki <M.rizkihakiki1997@gmail.com>
2024-10-28 15:40:07 +0700

    add db.php

--- portofolio.sql
+++ portofolio.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2024 at 04:59 PM
+-- Generation Time: Oct 28, 2024 at 09:39 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `update_at`, `created_at`) VALUES
+(1, 'admin123', 'admin@gmail.com', '1234', '2024-10-28 03:41:46', '2024-10-28 03:41:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `capabilitas`
+--
+
+CREATE TABLE `capabilitas` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `paragraf` varchar(255) NOT NULL,
+  `info` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `capabilitas`
+--
+
+INSERT INTO `capabilitas` (`id`, `judul`, `paragraf`, `info`, `created_at`) VALUES
+(2, 'rindu yang terkubur', 'lorem ipsum dolor site amet. amer123', 'baru dimulai', '2024-10-28 07:54:14'),
+(3, 'azab api gas LPG', 'lorem ipsum dolor site amet. amer123', 'baru dimulai', '2024-10-28 08:05:06');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `konten`
 --
 
@@ -36,6 +80,13 @@ CREATE TABLE `konten` (
   `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `create_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `konten`
+--
+
+INSERT INTO `konten` (`id`, `judul_konten`, `isi_konten`, `foto`, `keterangan`, `update_at`, `create_at`) VALUES
+(2, 'Elysia', 'hacenser of human', 'picture8.jpg', 'lorem ipsum dolor', '2024-10-28 01:19:37', '2024-10-28 01:17:13');
 
 -- --------------------------------------------------------
 
@@ -57,11 +108,24 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `alamat`, `foto`, `update_at`, `create_at`) VALUES
-(1, 'rio tunsteng', 'bandung', '1210822.png', '2024-10-26 16:38:31', '2024-10-26 14:35:11');
+(1, 'rio tunsteng', 'bandung', '1210822.png', '2024-10-26 16:38:31', '2024-10-26 14:35:11'),
+(5, 'sparkel', 'penacony', 'picture4.jpg', '2024-10-28 06:28:44', '2024-10-28 06:28:44');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `capabilitas`
+--
+ALTER TABLE `capabilitas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `konten`
@@ -80,16 +144,28 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `capabilitas`
+--
+ALTER TABLE `capabilitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `konten`
 --
 ALTER TABLE `konten`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
